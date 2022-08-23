3764bd4e9a12ef85cd17c48eea94789a60853029
Shawn102~ <shawnniamul58@gmail.com>
2022-08-23 14:45:19 +0800

    github login redirect url update

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3500/auth/github/submit",
+      callbackURL: "https://secret-todolist.herokuapp.com/auth/github/submit",
     },
     function (accessToken, refreshToken, profile, done) {
       User.findOrCreate({ githubId: profile.id }, function (err, user) {
